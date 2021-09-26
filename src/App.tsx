96b34c45ceeea0a0bc1a6ff8df03a6e68c2e0c2b
toshi <to4king@gmail.com>
2021-09-26 23:50:21 +0900

    add sample chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
+import "./App.css";
+
+const CHART_SETTINGS = {
+  chartType: "Timeline",
+  width: "500px",
+  height: "300px",
+  options: { showRowNumber: true },
+} as const;
+
+const DATA_DEFINITIONS = [
+  { type: "string", id: "agency-name" },
+  { type: "string", id: "group-name" },
+  { type: "date", id: "Start" },
+  { type: "date", id: "End" },
+] as const;
+
+const dataRows = [
+  "BIGHIT MUSIC",
+  "BTS(防弾少年団)",
+  new Date(2013, 6, 13),
+  new Date(),
+];
+
+const chartData = [DATA_DEFINITIONS, dataRows];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+        width={CHART_SETTINGS.width}
+        height={CHART_SETTINGS.height}
+        chartType={CHART_SETTINGS.chartType}
+        options={CHART_SETTINGS.options}
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        rootProps={{ "data-testid": "1" }}
+      />
     </div>
   );
 }
