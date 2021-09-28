784f99186affabf696e42ed348e7159bc6696c2a
toshi <to4king@gmail.com>
2021-09-29 02:50:46 +0900

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import Timeline from "react-visjs-timeline";
-
-const options = {
-  width: "100%",
-  height: "100px",
-  stack: false,
-  showMajorLabels: true,
-  showCurrentTime: true,
-  zoomMin: 1000000,
-  type: "background",
-  format: {
-    minorLabels: {
-      minute: "h:mma",
-      hour: "ha",
-    },
-  },
-};
-
-const items = [
-  {
-    start: new Date(2021, 8, 15),
-    end: new Date(2021, 9, 2), // end is optional
-    content: "Trajectory A",
-  },
-];
-
 function App() {
-  return (
-    <div className="App">
-      <Timeline options={options} items={items} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
