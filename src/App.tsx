61b707d4b07db3b866dfa6f762fba052b5d5513f
toshi <to4king@gmail.com>
2021-10-03 01:01:38 +0900

    add sample timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from "react";
-import "./App.css";
+import { Timeline } from "react-svg-timeline";
 
 function App() {
-  return <div className="App"></div>;
+  const laneId = "demo-lane";
+  const lanes = [
+    {
+      laneId,
+      label: "Demo Lane",
+    },
+  ];
+  const events = [
+    {
+      eventId: "event-1",
+      laneId: laneId,
+      startTimeMillis: 1399845600000,
+    },
+    {
+      eventId: "event-2",
+      laneId,
+      startTimeMillis: 1167606000000,
+      endTimeMillis: 1230698892000,
+    },
+  ];
+  const dateFormat = (ms: number) => new Date(ms).toLocaleString();
+  return (
+    <Timeline
+      width={600}
+      height={300}
+      events={events}
+      lanes={lanes}
+      dateFormat={dateFormat}
+    />
+  );
 }
 
 export default App;
