aca44d10e21ee9c9e19923edea46001af6947169
toshi <to4king@gmail.com>
2021-10-03 02:20:13 +0900

    add sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import { Timeline } from "react-svg-timeline";
+import agency from "./data/agency.json";
+import group from "./data/group.json";
 
 function App() {
-  const laneId = "demo-lane";
-  const lanes = [
-    {
-      laneId,
-      label: "Demo Lane",
-    },
-  ];
-  const events = [
-    {
-      eventId: "event-1",
-      laneId: laneId,
-      startTimeMillis: 1399845600000,
-    },
-    {
-      eventId: "event-2",
-      laneId,
-      startTimeMillis: 1167606000000,
-      endTimeMillis: 1230698892000,
-    },
-  ];
+  const lanes = agency.agencies;
+  const events = group.groups;
   const dateFormat = (ms: number) => new Date(ms).toLocaleString();
   return (
     <Timeline
